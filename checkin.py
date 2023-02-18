16ca1a7d2c06af9577ff8e903c1c56bbbace8adf
zhule <zhuleaiziji@163.com>
2023-02-18 14:41:12 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -37,7 +37,9 @@ def do_action():
     print('*******************************************')
     if push_plus_token:
       push_result = requests.request("GET", 'http://www.pushplus.plus/send?template=html&token=' + push_plus_token+ '&title=CheckIn&content=' + message, data = payload)
-      print(push_result)
+      print(push_result.text)
+      print(push_plus_token)
+      push_plus_token
     return result
 
 
